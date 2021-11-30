87db2d7bedbe047fbb0e70485376bf54e5270281
Aleksey Pakhomov <Alexey007-89@mail.ru>
2021-11-30 20:22:25 +1000

    Исправлены замечания

--- src/main/java/pro/sky/java/course2/webcalculator/CalculatorController.java
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorController.java
@@ -16,27 +16,31 @@ public class CalculatorController {
 
     @GetMapping
     public String greeting() {
-        return calculatorService.greeting();
+        return "Добро пожаловать в калькулятор";
     }
 
     @GetMapping("/plus")
     public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.plus(num1, num2);
+        return num1 + " + " + num2 + " = " + calculatorService.plus(num1,num2);
     }
 
     @GetMapping("/minus")
     public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.minus(num1, num2);
+        return num1 + " - " + num2 + " = " + calculatorService.minus(num1,num2);
     }
 
     @GetMapping("/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.multiply(num1, num2);
+        return num1 + " * " + num2 + " = " + calculatorService.multiply(num1,num2);
     }
 
     @GetMapping("/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.divide(num1, num2);
+        if (num2 == 0) {
+            return "Деление на ноль невозможно";
+        } else {
+            return num1 + " / " + num2 + " = " + (num1 / num2);
+        }
     }
 }
 
--- src/main/java/pro/sky/java/course2/webcalculator/CalculatorService.java
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorService.java
@@ -1,13 +1,12 @@
 package pro.sky.java.course2.webcalculator;
 
 public interface CalculatorService {
-    String greeting();
 
-    String plus(int num1, int num2);
+    int plus(int num1, int num2);
 
-    String minus(int num1, int num2);
+    int minus(int num1, int num2);
 
-    String multiply(int num1, int num2);
+    int divide(int num1, int num2);
 
-    String divide(int num1, int num2);
+    int multiply(int num1, int num2);
 }
