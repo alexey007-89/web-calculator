e4f7b706b0be464dd85104373901903bee16cdd0
Aleksey Pakhomov <Alexey007-89@mail.ru>
2021-11-30 18:45:54 +1000

    Для проверки ДЗ

--- src/main/java/pro/sky/java/course2/webcalculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorServiceImpl.java
@@ -4,32 +4,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
-    @Override
-    public String greeting() {
-        return "Добро пожаловать в калькулятор";
-    }
 
-    @Override
-    public String plus(int num1, int num2) {
-        return num1 + " + " + num2 + " = " + (num1 + num2);
-    }
-
-    @Override
-    public String minus(int num1, int num2) {
-        return num1 + " - " + num2 + " = " + (num1 - num2);
-    }
-
-    @Override
-    public String divide(int num1, int num2) {
-        if (num2 == 0) {
-            return "Деление на ноль невозможно";
-        } else {
-            return num1 + " / " + num2 + " = " + (num1 / num2);
-        }
-    }
-
-    @Override
-    public String multiply(int num1, int num2) {
-        return num1 + " * " + num2 + " = " + (num1 * num2);
-    }
 }
