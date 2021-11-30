a0cd88e68a8bc60e63c18b376d8e8e9d963a3c9e
Aleksey Pakhomov <Alexey007-89@mail.ru>
2021-11-30 18:33:04 +1000

    Для проверки ДЗ

--- src/main/java/pro/sky/java/course2/webcalculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorServiceImpl.java
@@ -21,7 +21,11 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public String divide(int num1, int num2) {
-        return num1 + " / " + num2 + " = " + (num1 / num2);
+        if (num2 == 0) {
+            return "Деление на ноль невозможно";
+        } else {
+            return num1 + " / " + num2 + " = " + (num1 / num2);
+        }
     }
 
     @Override
