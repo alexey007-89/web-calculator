de1400b28dfef0edfb5d8f0a8bfa764cf2610efb
Aleksey Pakhomov <Alexey007-89@mail.ru>
2021-11-30 16:16:55 +1000

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorService.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webcalculator;
+
+public interface CalculatorService {
+    String greeting();
+
+    String plus(int num1, int num2);
+
+    String minus(int num1, int num2);
+
+    String multiply(int num1, int num2);
+
+    String divide(int num1, int num2);
+}
---
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorServiceImpl.java
@@ -0,0 +1,31 @@
+package pro.sky.java.course2.webcalculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public String greeting() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @Override
+    public String plus(int num1, int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    @Override
+    public String minus(int num1, int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    @Override
+    public String divide(int num1, int num2) {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+
+    @Override
+    public String multiply(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+}
